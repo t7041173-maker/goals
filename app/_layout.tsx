6060d9371a6fc66e4a736b7aa08190d079a8deeb
t7041173-maker <t7041173@gmail.com>
2025-08-04 11:49:36 +0530

    Fix App and Add Money Feature

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,13 @@ import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
+import { GoalsProvider } from '@/contexts/GoalsContext';
 
 export default function RootLayout() {
   useFrameworkReady();
 
   return (
-    <>
+    <GoalsProvider>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
         <Stack.Screen name="all-goals" />
@@ -15,6 +16,6 @@ export default function RootLayout() {
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
-    </>
+    </GoalsProvider>
   );
 }
